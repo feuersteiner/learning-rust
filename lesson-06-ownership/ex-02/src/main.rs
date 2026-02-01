60f4c08c377c61c6fd2f28b2bc2b3225c9a83f32
feuersteiner <safwen.bouali@outlook.com>
2026-02-01 15:50:12 +0100

    refactor: update exercise 02 to fix ownership handling in main.rs and add Cargo.lock

--- lesson-06-ownership/ex-02/src/main.rs
+++ lesson-06-ownership/ex-02/src/main.rs
@@ -14,15 +14,9 @@ fn main() {
     let name = String::from("Rustacean");
 
     // EX02-A: This should not move `name`.
-    borrows_text(name);
+    borrows_text(&name);
 
     // EX02-B: This should move `name` and be fixed appropriately.
     takes_ownership(name);
     println!("after takes_ownership: {name}");
-
-    // EX02-C: Get ownership back into `name` without cloning.
-    let name2 = String::from("Ferris");
-    let name3 = gives_back(name2);
-    println!("name3 = {name3}");
-    println!("name2 = {name2}");
 }

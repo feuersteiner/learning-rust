7efd755e2cf526df3be1fd71b487ba77de64074b
feuersteiner <safwen.bouali@outlook.com>
2026-02-01 19:07:52 +0100

    read words

--- lesson-06-ownership/ex-03/src/main.rs
+++ lesson-06-ownership/ex-03/src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let words: Vec<String> = input
+        .trim()
+        .split_whitespace()
+        .map(|i| i.to_string())
+        .collect();
+
+    println!("{words:?}");
 }
